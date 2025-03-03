bcd397ecdb2b8036332f9a5532fc2b194e683c2d
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2025-03-03 09:49:36 +0100

    refactor: switch to button toggle

--- popup.js
+++ popup.js
@@ -1,27 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleSwitch = document.getElementById('toggleSwitch');
-    const offLabel = document.getElementById('offLabel');
-    const onLabel = document.getElementById('onLabel');
-
-    // Initialize the labels based on the switch state
-    updateLabels(toggleSwitch.checked);
-
-    toggleSwitch.addEventListener('change', (event) => {
-        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-            chrome.tabs.sendMessage(tabs[0].id, {action: "toggleCSS"}, (response) => {
-                console.log(response.status);
-                updateLabels(event.target.checked);
-            });
+document.getElementById('toggleButton').addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "toggleCSS"}, (response) => {
+            console.log(response.status);
         });
     });
-
-    function updateLabels(isChecked) {
-        if (isChecked) {
-            offLabel.style.color = '#ccc';
-            onLabel.style.color = '#000';
-        } else {
-            offLabel.style.color = '#000';
-            onLabel.style.color = '#ccc';
-        }
-    }
 });
